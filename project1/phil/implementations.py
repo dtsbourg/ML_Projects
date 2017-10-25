27dd44e689aaa233c4f13419bec0b705af45dcd0
Philippe Verbist <verbist.philippe@gmail.com>
2017-10-25 03:11:33 +0200

    deep review of the code + first submission

---
+++ project1/phil/implementations.py
@@ -0,0 +1,161 @@
+import numpy as np
+from auxiliary import *
+
+# Contains all methods asked in step 2
+
+def least_squares(y, tx):
+    """
+    Linear regresison using normal equations
+    Returns optimal weights and associated minimum mse loss
+    """
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+
+    w = np.linalg.solve(a, b)
+    loss = compute_loss(y, tx, w)
+    return w, loss
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    """
+    Linear regression using gradient descent
+    Returns optimal weights and associated minimum loss
+    """
+    w_start = initial_w
+    w = w_start
+
+    for n_iter in range(max_iters):
+        gradient = compute_gradient(y, tx, w)
+        loss = compute_loss(y,tx,w)
+        w = w - gamma * gradient
+
+    return w, loss
+
+def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
+    """
+    Linear regression using stochastic gradient descent
+    Returns optimal weights and associated minimum loss
+    """
+    w = initial_w
+    loss = compute_loss(y, tx, w)
+
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size, max_iters):
+        loss = compute_loss(y, tx, w) # avant: minibatch_y et minibatch_tx
+        gradients = compute_gradient(minibatch_y, minibatch_tx, w)
+        w = w - gamma * gradients
+
+    return w, loss
+
+def ridge_regression(y, tx, lambda_):
+    """
+    Ridge regression using normal equations
+    Returns optimal weights and associated minimum loss
+    """
+    lambda_prime = lambda_ / (2*tx.shape[0])
+    a = np.linalg.inv(tx.transpose().dot(tx) + lambda_*np.eye(tx.shape[1]))
+    
+    b = tx.transpose().dot(y)
+    w_star = a.dot(b)
+    
+    e = y - tx.dot(w_star)
+    
+    mse = np.asarray((e**2).mean())
+    return w_star, mse
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma, SGD=False, batch_size=-1) :
+    """
+    Logistic regression using gradient descent
+    Returns optimal weights and associated minimum loss
+    """
+    w_start = initial_w
+    w = w_start
+    loss_old = 0.0
+
+    for n_iter in range(max_iters):
+        loss = compute_logistic_loss(y, tx, w)
+        gradient = compute_logistic_gradient(y, tx, w)
+        w = w - gamma * gradient
+        
+        if check_stop(loss, loss_old):
+            print('break!')
+            w = w_new
+            break;
+        loss_old = loss
+
+    return w, loss
+
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma, SGD=False, batch_size=-1):
+    """
+    Regularized logistic regression using gradient descent
+    Returns optimal weights and associated minimum loss
+    """
+    w_start = initial_w
+    w = w_start
+    loss_old = 0.0
+    
+    if SGD:
+        if(batch_size==-1): # compute automatically the maximum batch size
+            batch_size = int(y.shape[0]/max_iters) 
+        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size, max_iters):
+            loss = compute_loss(minibatch_y, minibatch_tx, w)
+            gradient = compute_gradient(minibatch_y, minibatch_tx, w)
+            loss_reg, gradient_reg = regularizer(lambda_, w)
+            loss = loss + loss_reg
+            gradient = gradient + gradient_reg
+            w = w - gamma * gradient
+            
+            if check_stop(loss, loss_old):
+                print('break!')
+                break;
+            loss_old = loss
+        return w, loss
+    
+    else:
+        for n_iter in range(max_iters):
+            loss = compute_logistic_loss(y, tx, w)
+            gradient = compute_logistic_gradient(y, tx, w)
+            loss_reg, gradient_reg = regularizer(lambda_, w)
+            loss_new = loss + loss_reg
+            gradient = gradient + gradient_reg
+            w = w - gamma * gradient
+            
+            if check_stop(loss, loss_old):
+                print('break!')
+                break;
+            loss_old = loss
+        return w, loss
+
+def logistic_regression_SGD(y, tx, initial_w, max_iters, gamma, batch_size) :
+    """
+    Logistic regression using stochastic gradient descent
+    Returns optimal weights and associated minimum loss
+    """
+    w_start = initial_w
+    w = w_start
+
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size, max_iters):
+        loss = compute_loss(minibatch_y, minibatch_tx, w)
+        gradients = compute_gradient(minibatch_y, minibatch_tx, w)
+
+        w = w - [gamma * g for g in gradients]
+
+    return w, loss
+
+def reg_logistic_regression_SGD(y, tx, lambda_, initial_w, max_iters, gamma):
+    """
+    Regularized logistic regression using stochastic gradient descent
+    Returns optimal weights and associated minimum loss
+    """
+    w_start = initial_w
+    w = w_start
+
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size, max_iters):
+        loss = compute_loss(minibatch_y, minibatch_tx, w)
+        gradients = compute_gradient(minibatch_y, minibatch_tx, w)
+        loss_reg, gradient_reg = regularizer(lambda_, w)
+
+        loss = loss + loss_reg
+        gradient = gradient + gradient_reg
+
+        w = w - [gamma * g for g in gradients]
+
+    return w, loss
---
+++ project1/phil/proj1_helpers.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""some helper functions for project 1."""
+import csv
+import numpy as np
+
+
+def load_csv_data(data_path, sub_sample=False): # OK
+    """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
+    y = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
+    x = np.genfromtxt(data_path, delimiter=",", skip_header=1)
+    ids = x[:, 0].astype(np.int)
+    input_data = x[:, 2:]
+
+    # convert class labels from strings to binary (0,1)
+    yb = np.ones(len(y))
+    yb[np.where(y=='b')] = 0
+    
+    # sub-sample
+    if sub_sample:
+        yb = yb[::50]
+        input_data = input_data[::50]
+        ids = ids[::50]
+
+    return ids, yb, input_data
+
+
+def load_easy_data(sub_sample=False, add_outlier=False): # OK
+    """Load data and convert it to the metrics system."""
+    path_dataset = "height_weight_genders.csv"
+    data = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[1, 2])
+    height = data[:, 0]
+    weight = data[:, 1]
+    gender = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[0],
+        converters={0: lambda x: 0 if b"Male" in x else 1})
+    # Convert to metric system
+    height *= 0.025
+    weight *= 0.454
+
+    # sub-sample
+    if sub_sample:
+        height = height[::50]
+        weight = weight[::50]
+        gender = gender[::50]
+
+    if add_outlier:
+        # outlier experiment
+        height = np.concatenate([height, [1.1, 1.2]])
+        weight = np.concatenate([weight, [51.5/0.454, 55.3/0.454]])
+
+    ids = [x for x in range(gender.shape[0])]
+    x = np.column_stack((height, weight))
+
+
+    return ids, gender, x
+
+
+def predict_labels(weights, data): # OK
+    """Generates class predictions given weights, and a test data matrix"""
+    y_pred = np.dot(data, weights)
+    y_pred[np.where(y_pred <= 0)] = 0
+    y_pred[np.where(y_pred > 0)] = 1
+    
+    return y_pred
+
+
+def create_csv_submission(ids, y_pred, name): # OK
+    """
+    Creates an output file in csv format for submission to kaggle
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
+    """
+    with open(name, 'w') as csvfile:
+        fieldnames = ['Id', 'Prediction']
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({'Id':int(r1),'Prediction':int(r2)})
