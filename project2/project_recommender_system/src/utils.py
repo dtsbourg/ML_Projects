544cac88ceba60ec5add5163e976adc0a259e1a3
dtsbourg <dtsbourg@gmail.com>
2017-12-03 20:09:04 +0100

    add manifold builder

--- project2/project_recommender_system/src/utils.py
+++ project2/project_recommender_system/src/utils.py
@@ -1,5 +1,7 @@
 from keras.utils import plot_model
 from keras import models
+import numpy as np
+from sklearn import manifold
 
 def save_model_graph(model):
     plot_model(model, to_file='../res/model/' + model.descr + '.png')
@@ -12,3 +14,25 @@ def save_model(cm):
 
 def load_model(path):
     return models.load_model(path)
+
+def build_interaction_matrix(users, items, ratings, suffix=''):
+    matrix = np.zeros((max(users), max(items)), dtype=np.int)
+
+    for i in range(len(users)):
+        matrix[users[i]-1, items[i]-1] = ratings[i]
+    np.save('../data/embeddings/interaction_matrix'+suffix, matrix)
+
+def build_embedding(path, embedding=None):
+    if embedding == 't-SNE':
+        mat = np.load(path)
+        tsne = manifold.TSNE(n_components=3, random_state=0)
+        u_tsne = tsne.fit_transform(mat)
+        i_tsne = tsne.fit_transform(mat.T)
+        return u_tsne, i_tsne
+
+def build_tSNE_embedding(path, suffix="", save=True):
+    u_e, i_e = build_embedding(path=path, embedding='t-SNE')
+    if save is True:
+        np.save('../data/embeddings/users_t_sne'+suffix, u_e)
+        np.save('../data/embeddings/items_t_sne'+suffix, i_e)
+    return u_e, i_e
