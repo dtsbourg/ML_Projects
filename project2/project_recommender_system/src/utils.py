c9a3a5936ef9c41d3b2eb443046a05d4c409f298
dtsbourg <dtsbourg@gmail.com>
2017-12-15 08:35:42 +0100

    new embeddings

--- project2/project_recommender_system/src/utils.py
+++ project2/project_recommender_system/src/utils.py
@@ -2,6 +2,7 @@ from keras.utils import plot_model
 from keras import models
 import numpy as np
 from sklearn import manifold
+from sklearn import decomposition
 
 def save_model_graph(model):
     plot_model(model, to_file='../res/model/' + model.descr + '.png')
@@ -32,14 +33,24 @@ def build_embedding(path, embedding=None):
         return u_tsne, i_tsne
     elif embedding == 'spectral':
         mat = np.load(path)
-        u_spectral = manifold.SpectralEmbedding(n_components=128, random_state=0, n_jobs=8).fit_transform(mat)
-        i_spectral = manifold.SpectralEmbedding(n_components=128, random_state=0, n_jobs=8).fit_transform(mat.T)
+        u_spectral = manifold.SpectralEmbedding(n_components=64, random_state=0, n_jobs=8).fit_transform(mat)
+        i_spectral = manifold.SpectralEmbedding(n_components=64, random_state=0, n_jobs=8).fit_transform(mat.T)
         return u_spectral, i_spectral
     elif embedding == 'lle':
         mat = np.load(path)
-        u_lle = manifold.LocallyLinearEmbedding(n_components=128, random_state=0, n_jobs=8).fit_transform(mat)
-        i_lle = manifold.LocallyLinearEmbedding(n_components=128, random_state=0, n_jobs=8).fit_transform(mat.T)
+        u_lle = manifold.LocallyLinearEmbedding(n_components=64, random_state=0, n_jobs=8).fit_transform(mat)
+        i_lle = manifold.LocallyLinearEmbedding(n_components=64, random_state=0, n_jobs=8).fit_transform(mat.T)
         return u_lle, i_lle
+    elif embedding == 'fa':
+        mat = np.load(path)
+        u_fa = decomposition.FactorAnalysis(n_components=64, random_state=0).fit_transform(mat)
+        i_fa = decomposition.FactorAnalysis(n_components=64, random_state=0).fit_transform(mat.T)
+        return u_fa, i_fa
+    elif embedding == 'nmf':
+        mat = np.load(path)
+        u_nmf = decomposition.NMF(n_components=64, random_state=0).fit_transform(mat)
+        i_nmf = decomposition.NMF(n_components=64, random_state=0).fit_transform(mat.T)
+        return u_nmf, i_nmf
 
 def build_tSNE_embedding(path, suffix="", save=True):
     u_e, i_e = build_embedding(path=path, embedding='t-SNE')
@@ -60,4 +71,18 @@ def build_lle_embedding(path, suffix="", save=True):
     if save is True:
         np.save('../data/embeddings/users_lle'+suffix, u_e)
         np.save('../data/embeddings/items_lle'+suffix, i_e)
+    return u_e, i_e
+
+def build_fa_embedding(path, suffix="", save=True):
+    u_e, i_e = build_embedding(path=path, embedding='fa')
+    if save is True:
+        np.save('../data/embeddings/users_fa'+suffix, u_e)
+        np.save('../data/embeddings/items_fa'+suffix, i_e)
+    return u_e, i_e
+
+def build_nmf_embedding(path, suffix="", save=True):
+    u_e, i_e = build_embedding(path=path, embedding='nmf')
+    if save is True:
+        np.save('../data/embeddings/users_nmf'+suffix, u_e)
+        np.save('../data/embeddings/items_nmf'+suffix, i_e)
     return u_e, i_e
