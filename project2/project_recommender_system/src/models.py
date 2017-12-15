9078e56a62a4cc4ed2f3644f906d5a81b6495541
dtsbourg <dtsbourg@gmail.com>
2017-12-15 11:02:02 +0100

    continue model cleanup

--- project2/project_recommender_system/src/models.py
+++ project2/project_recommender_system/src/models.py
@@ -24,7 +24,6 @@ from keras import optimizers
 from keras import regularizers
 import keras.backend as K
 import datetime
-
 import numpy as np
 
 
@@ -43,7 +42,9 @@ class Network(object):
         self.model = self.model_func()
         self.model_type = "Basic"
         self.descr = self.description_str()
-        self.embed_dim = None
+        self.EMBED_FEAT = 64
+        self.DROPOUT    = 0.5
+        self.EMBED_REG  = 1e-4
 
     def model_func(self):
         raise NotImplementedError()
@@ -67,7 +68,7 @@ class ShallowNetwork(Network):
     """docstring for ShallowNetwork."""
     def __init__(self, *args, **kwargs):
         super(ShallowNetwork, self).__init__(*args, **kwargs)
-        self.model_type = "Shallow"
+        self.model_type = "Shallow_Final"
 
     def model_func(self):
         input_i = layers.Input(shape=[1])
@@ -83,7 +84,7 @@ class ShallowNetwork(Network):
         nn = layers.concatenate([i, u])
 
         nn = layers.Dense(512, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
+        nn = layers.Dropout(self.DROPOUT)(nn)
         nn = layers.normalization.BatchNormalization()(nn)
 
         nn = layers.Dense(128, activation='relu')(nn)
@@ -94,220 +95,48 @@ class ShallowNetwork(Network):
         model.compile(optimizer=self.optimizer, loss=self.loss)
         return model
 
-class ShallowNetwork2(Network):
-    """docstring for ShallowNetwork2."""
-    def __init__(self, *args, **kwargs):
-        super(ShallowNetwork, self).__init__(*args, **kwargs)
-        self.model_type = "Shallow2"
-
-    def model_func(self):
-        input_i = layers.Input(shape=[1])
-        i = layers.Embedding(self.n_items+1, self.k_features, embeddings_regularizer=l2(0.0001))(input_i)
-        input_u = layers.Input(shape=[1])
-        u = layers.Embedding(self.n_users+1, self.k_features, embeddings_regularizer=l2(0.0001))(input_u)
-
-        nn = layers.concatenate([i, u])
-
-        nn = Flatten()(nn)
-        nn = Dropout(0.25)(nn)
-        nn = Dense(64, activation='relu')(nn)
-        nn = Dropout(0.75)(nn)
-
-        output = Dense(1)(nn)
-
-        model = models.Model([input_i, input_u], output)
-        model.compile(Adam(0.001), loss='mse')
-
-        return model
-
 
 class DeepNetwork(Network):
     """docstring for DeepNetwork."""
     def __init__(self, *args, **kwargs):
         super(DeepNetwork, self).__init__(*args, **kwargs)
-        self.model_type = "Simple_Deep"
+        self.model_type = "Deep_Full_Final"
 
     def model_func(self):
         input_i = layers.Input(shape=[1])
-        i = layers.Embedding(self.n_items + 1, self.k_features)(input_i)
+        i = layers.Embedding(self.n_items + 1, self.k_features, embeddings_regularizer=regularizers.l2(self.EMBED_REG))(input_i)
         i = layers.Flatten()(i)
-        i = layers.normalization.BatchNormalization()(i)
 
         input_u = layers.Input(shape=[1])
-        u = layers.Embedding(self.n_users + 1, self.k_features)(input_u)
+        u = layers.Embedding(self.n_users + 1, self.k_features, embeddings_regularizer=regularizers.l2(self.EMBED_REG))(input_u)
         u = layers.Flatten()(u)
-        u = layers.normalization.BatchNormalization()(u)
 
-        nn = layers.concatenate([i, u])
+        input_i_spectral = layers.Input(shape=[self.EMBED_FEAT])
+        input_u_spectral = layers.Input(shape=[self.EMBED_FEAT])
 
-        nn = layers.Dense(1024, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
-        nn = layers.normalization.BatchNormalization()(nn)
-        nn = layers.Dense(512, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
-        nn = layers.normalization.BatchNormalization()(nn)
-        nn = layers.Dense(256, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
-        nn = layers.normalization.BatchNormalization()(nn)
-        nn = layers.Dense(128, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
-        nn = layers.normalization.BatchNormalization()(nn)
-        nn = layers.Dense(64, activation='relu')(nn)
+        input_i_lle = layers.Input(shape=[self.EMBED_FEAT])
+        input_u_lle = layers.Input(shape=[self.EMBED_FEAT])
 
-        output = layers.Dense(5, activation='softmax')(nn)
+        input_i_fa = layers.Input(shape=[self.EMBED_FEAT])
+        input_u_fa = layers.Input(shape=[self.EMBED_FEAT])
 
-        model = models.Model([input_i, input_u], output)
-        model.compile(optimizer=self.optimizer, loss=self.loss, metrics=['accuracy'])
-        return model
-
-class DeepNetworkFeat(Network):
-    """docstring for DeepNetworkFeat."""
-    def __init__(self, *args, **kwargs):
-        super(DeepNetworkFeat, self).__init__(*args, **kwargs)
-        self.model_type = "Deep_Full_Feat"
-
-    def model_func(self):
-        input_i = layers.Input(shape=[1])
-        i = layers.Embedding(self.n_items + 1, self.k_features)(input_i)
-        i = layers.Flatten()(i)
-        i = layers.normalization.BatchNormalization()(i)
-
-        input_u = layers.Input(shape=[1])
-        u = layers.Embedding(self.n_users + 1, self.k_features)(input_u)
-        u = layers.Flatten()(u)
-        u = layers.normalization.BatchNormalization()(u)
-
-        input_i_spectral = layers.Input(shape=[128])
-        im_spectral = layers.normalization.BatchNormalization()(input_i_spectral)
-        input_u_spectral = layers.Input(shape=[128])
-        um_spectral = layers.normalization.BatchNormalization()(input_u_spectral)
-
-        input_i_lle = layers.Input(shape=[128])
-        im_lle = layers.normalization.BatchNormalization()(input_i_lle)
-        input_u_lle = layers.Input(shape=[128])
-        um_lle = layers.normalization.BatchNormalization()(input_u_lle)
-
-        nn = layers.concatenate([i, u, im_spectral, um_spectral, im_lle, um_lle])
-
-        nn = layers.Dense(1024, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
-        nn = layers.normalization.BatchNormalization()(nn)
-        nn = layers.Dense(512, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
-        nn = layers.normalization.BatchNormalization()(nn)
-        nn = layers.Dense(256, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
-        nn = layers.normalization.BatchNormalization()(nn)
-        nn = layers.Dense(128, activation='relu')(nn)
-
-        output = layers.Dense(5, activation='softmax')(nn)
-
-        model = models.Model([input_i, input_u, input_i_spectral, input_u_spectral, input_i_lle, input_u_lle], output)
-        model.compile(optimizer=self.optimizer, loss=self.loss, metrics=['accuracy'])
-        return model
-
-
-
-# class DeepNetworkFeatRegCustLoss(Network):
-#     """docstring for DeepNetworkFeatCustLoss."""
-#     def __init__(self, *args, **kwargs):
-#         super(DeepNetworkFeatRegCustLoss, self).__init__(*args, **kwargs)
-#         self.model_type = "Deep_Full_Feat_RegCustom_Loss"
-#
-#     def customLoss(y_true,y_pred):
-#         return K.sum(K.log(yTrue) - K.log(yPred))
-#
-#     def model_func(self):
-#         input_i = layers.Input(shape=[1])
-#         i = layers.Embedding(self.n_items + 1, self.k_features)(input_i)
-#         i = layers.Flatten()(i)
-#         i = layers.normalization.BatchNormalization()(i)
-#
-#         input_u = layers.Input(shape=[1])
-#         u = layers.Embedding(self.n_users + 1, self.k_features)(input_u)
-#         u = layers.Flatten()(u)
-#         u = layers.normalization.BatchNormalization()(u)
-#
-#         input_i_spectral = layers.Input(shape=[128])
-#         im_spectral = layers.normalization.BatchNormalization()(input_i_spectral)
-#         input_u_spectral = layers.Input(shape=[128])
-#         um_spectral = layers.normalization.BatchNormalization()(input_u_spectral)
-#
-#         input_i_lle = layers.Input(shape=[128])
-#         im_lle = layers.normalization.BatchNormalization()(input_i_lle)
-#         input_u_lle = layers.Input(shape=[128])
-#         um_lle = layers.normalization.BatchNormalization()(input_u_lle)
-#
-#         nn = layers.concatenate([i, u, im_spectral, um_spectral, im_lle, um_lle])
-#
-#         nn = layers.Dense(1024, activation='relu', activity_regularizer=regularizers.l2(0.001))(nn)
-#         nn = layers.Dropout(0.5)(nn)
-#         nn = layers.normalization.BatchNormalization()(nn)
-#         nn = layers.Dense(512, activation='relu', activity_regularizer=regularizers.l2(0.001))(nn)
-#         nn = layers.Dropout(0.5)(nn)
-#         nn = layers.normalization.BatchNormalization()(nn)
-#         nn = layers.Dense(256, activation='relu', activity_regularizer=regularizers.l2(0.001))(nn)
-#         nn = layers.Dropout(0.5)(nn)
-#         nn = layers.normalization.BatchNormalization()(nn)
-#         nn = layers.Dense(128, activation='relu', activity_regularizer=regularizers.l2(0.001))(nn)
-#
-#         output = layers.Dense(5, activation='softmax')(nn)
-#
-#         model = models.Model([input_i, input_u, input_i_spectral, input_u_spectral, input_i_lle, input_u_lle], output)
-#         model.compile(optimizer=self.optimizer, loss=self.loss, metrics=['accuracy'])
-#         return model
-
-class DeepNetworkFeatReg(Network):
-    """docstring for DeepNetworkFeat."""
-    def __init__(self, *args, **kwargs):
-        super(DeepNetworkFeatReg, self).__init__(*args, **kwargs)
-        self.model_type = "Deep_Full_Feat_Reg"
-
-    def model_func(self):
-        input_i = layers.Input(shape=[1])
-        i = layers.Embedding(self.n_items + 1, self.k_features, embeddings_regularizer=regularizers.l2(0.0001))(input_i)
-        i = layers.Flatten()(i)
-        i = layers.normalization.BatchNormalization()(i)
-
-        input_u = layers.Input(shape=[1])
-        u = layers.Embedding(self.n_users + 1, self.k_features, embeddings_regularizer=regularizers.l2(0.0001))(input_u)
-        u = layers.Flatten()(u)
-        u = layers.normalization.BatchNormalization()(u)
-
-        input_i_spectral = layers.Input(shape=[64])
-        im_spectral = layers.normalization.BatchNormalization()(input_i_spectral)
-        input_u_spectral = layers.Input(shape=[64])
-        um_spectral = layers.normalization.BatchNormalization()(input_u_spectral)
-
-        input_i_lle = layers.Input(shape=[64])
-        im_lle = layers.normalization.BatchNormalization()(input_i_lle)
-        input_u_lle = layers.Input(shape=[64])
-        um_lle = layers.normalization.BatchNormalization()(input_u_lle)
-
-        input_i_fa = layers.Input(shape=[64])
-        im_fa = layers.normalization.BatchNormalization()(input_i_fa)
-        input_u_fa = layers.Input(shape=[64])
-        um_fa = layers.normalization.BatchNormalization()(input_u_fa)
-
-        input_i_nmf = layers.Input(shape=[64])
-        im_nmf = layers.normalization.BatchNormalization()(input_i_nmf)
-        input_u_nmf = layers.Input(shape=[64])
-        um_nmf = layers.normalization.BatchNormalization()(input_u_nmf)
+        input_i_nmf = layers.Input(shape=[self.EMBED_FEAT])
+        input_u_nmf = layers.Input(shape=[self.EMBED_FEAT])
 
         nn = layers.concatenate([i, u,
-                                 im_spectral, um_spectral,
-                                 im_lle, um_lle,
-                                 im_fa, um_fa,
-                                 im_nmf, um_nmf])
+                                 input_i_spectral, input_u_spectral,
+                                 input_i_lle, input_u_lle,
+                                 input_i_fa, input_u_fa,
+                                 input_i_nmf, input_u_nmf])
 
         nn = layers.Dense(1024, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
+        nn = layers.Dropout(self.DROPOUT)(nn)
         nn = layers.normalization.BatchNormalization()(nn)
         nn = layers.Dense(512, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
+        nn = layers.Dropout(self.DROPOUT)(nn)
         nn = layers.normalization.BatchNormalization()(nn)
         nn = layers.Dense(256, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
+        nn = layers.Dropout(self.DROPOUT)(nn)
         nn = layers.normalization.BatchNormalization()(nn)
         nn = layers.Dense(128, activation='relu')(nn)
 
@@ -326,34 +155,46 @@ class DenseNetwork(Network):
     """docstring for ShallowNetwork."""
     def __init__(self, *args, **kwargs):
         super(DenseNetwork, self).__init__(*args, **kwargs)
-        self.model_type = "Simple_Dense"
+        self.model_type = "Dense_Final"
 
     def model_func(self):
         input_i = layers.Input(shape=[1])
-        i = layers.Embedding(self.n_items + 1, self.k_features, embeddings_regularizer=regularizers.l2(0.0001))(input_i)
-        i = layers.Reshape((self.k_features,))(i)
-        i = layers.Dropout(0.5)(i)
-        # i = layers.normalization.BatchNormalization()(i)
+        i = layers.Embedding(self.n_items + 1, self.k_features, embeddings_regularizer=regularizers.l2(self.EMBED_REG))(input_i)
+        i = layers.Flatten()(i)
 
         input_u = layers.Input(shape=[1])
-        u = layers.Embedding(self.n_users + 1, self.k_features, embeddings_regularizer=regularizers.l2(0.0001))(input_u)
-        u = layers.Reshape((self.k_features,))(u)
-        u = layers.Dropout(0.5)(u)
-        # u = layers.normalization.BatchNormalization()(u)
+        u = layers.Embedding(self.n_users + 1, self.k_features, embeddings_regularizer=regularizers.l2(self.EMBED_REG))(input_u)
+        u = layers.Flatten()(u)
 
-        nn = layers.concatenate([i, u])
+        input_i_spectral = layers.Input(shape=[self.EMBED_FEAT])
+        input_u_spectral = layers.Input(shape=[self.EMBED_FEAT])
+
+        input_i_lle = layers.Input(shape=[self.EMBED_FEAT])
+        input_u_lle = layers.Input(shape=[self.EMBED_FEAT])
+
+        input_i_fa = layers.Input(shape=[self.EMBED_FEAT])
+        input_u_fa = layers.Input(shape=[self.EMBED_FEAT])
+
+        input_i_nmf = layers.Input(shape=[self.EMBED_FEAT])
+        input_u_nmf = layers.Input(shape=[self.EMBED_FEAT])
+
+        nn = layers.concatenate([i, u,
+                                 input_i_spectral, input_u_spectral,
+                                 input_i_lle, input_u_lle,
+                                 input_i_fa, input_u_fa,
+                                 input_i_nmf, input_u_nmf])
 
         nn = layers.Dense(256, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
+        nn = layers.Dropout(self.DROPOUT)(nn)
         nn = layers.normalization.BatchNormalization()(nn)
 
         nn = layers.Dense(256, activation='relu')(nn)
-        nn = layers.Dropout(0.5)(nn)
+        nn = layers.Dropout(self.DROPOUT)(nn)
         nn = layers.normalization.BatchNormalization()(nn)
 
         nn = layers.Dense(256, activation='relu')(nn)
 
-        output =  layers.Dense(self.n_classes, activation='softmax')(nn)
+        output = layers.Dense(self.n_classes, activation='softmax')(nn)
 
         model = models.Model([input_i, input_u], output)
 
