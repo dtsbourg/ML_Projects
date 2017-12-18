ecb74b2cce43472a17944bd13c17bee5b0db3dfd
dtsbourg <dtsbourg@gmail.com>
2017-12-18 09:43:34 +0100

    docstring run

--- project2/project_recommender_system/src/run.py
+++ project2/project_recommender_system/src/run.py
@@ -24,6 +24,19 @@ import numpy as np
 from utils import load_embedding, load_full_embedding
 
 def fit(model, train_x, train_y, test_x, test_y, embedding=False, epochs=10, batch_size=512):
+    """
+    Run the fitting procedure on the training data.
+
+    Args:
+        model (Network): wrapper for the Keras model. see `models.py`
+        train_x, train_y (list): training data
+        test_x, test_y (list): test data
+        embedding (bool): Are we using the embeddings ?
+        epochs (int): Number of epochs
+        batch_size (int): Batch size
+    Returns:
+        history (Keras.History): hold the training history (see https://keras.io/callbacks/#history)
+    """
     training_data = [train_x.Item, train_x.User]
     test_data = [test_x.Item, test_x.User]
 
@@ -44,6 +57,18 @@ def fit(model, train_x, train_y, test_x, test_y, embedding=False, epochs=10, bat
     return history
 
 def predict(categorical_predictions, voting='weighted'):
+    """
+    Run the prediction procedure on the training data.
+
+    Args:
+        categorical_predictions ([float]): the predictions from the model. For each
+                                           there are 5 confidence values (one per class)
+        voting (str): The voting scheme used :
+                        * 'weighted' = scale the class predictions by their confidence
+                        * 'absolute' = choose the class with the highest confidence
+    Returns:
+        ratings ([float]): The predicted rating
+    """
     if voting=='absolute':
         return [np.argmax(p)+1 for p in categorical_predictions]
     elif voting=='weighted':
